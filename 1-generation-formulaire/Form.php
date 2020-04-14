d67f12a111815a435cd1743a98465a475fa9a9c3
Paul <paulberthelot10@gmail.com>
2020-04-14 16:16:08 +0200

    versionPaul exercice pdo

--- 1-generation-formulaire/Form.php
+++ 1-generation-formulaire/Form.php
@@ -1,4 +1,39 @@
 <?php
 class Form{
+   public $action;
+   public $method;
+   public $name;
+   public $value;
+   public $html;
+   
+   public function __construct($action,$method)
+   {
+       $this->action = $action;
+       $this->method = $method;
+       $this->html = $this->addForm();
+   }
+
+   public function addForm() 
+   {
+       return "<form action=''.$this->action.'method=''.$this->method.''>";
+   }
+
+   public function addTextField(string $nom, string $prenom)    
+   {
+       $this->html .="<input type='text' name='' .$name.'' value=''.$value.''>";
+       return $this;
+   }
+
+   public function addSubmitButton() 
+   {
+    $this->html .= "<input type='submit' value=$value >";
+    return $this;
+   }
+
+   public function build() {
+    return $this->html."</form>";
+}
     
 }
+
+
